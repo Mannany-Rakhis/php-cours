576805fb707a6fa5654fbeec13f3253b1ff6bef6
Izayoii <rakhisahmat3600@gmail.com>
2026-05-12 14:53:54 +0200

    feat :  boolean exercice

---
+++ pair-impair.php
@@ -0,0 +1,11 @@
+<?php
+$nombre = 10;
+if ($nombre % 5) {
+    echo "$nombre est impair";
+} else {
+    echo "$nombre est pair";
+}   
+
+  $reste = $nombre % 2;
+  echo " Le reste de la division est : $reste";
+?>
