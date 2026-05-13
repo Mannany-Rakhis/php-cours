8ce41469d5940b6337eec477c6f757e4ad1070b5
Izayoii <rakhisahmat3600@gmail.com>
2026-05-13 09:53:39 +0200

    feat : mention bac

---
+++ mentionBac.php
@@ -0,0 +1,24 @@
+<?php
+
+$note = 14;
+ 
+if ($note >= 20) {
+    echo"Très bien !";
+
+} elseif ($note >= 16) {
+    echo "Bien";
+
+} elseif ($note >= 14) {
+    echo "Assez Bien";
+
+} elseif ($note >= 10) {
+
+    echo "Passable";
+
+} else {
+
+    echo "Refusé";
+
+}
+
+?>
